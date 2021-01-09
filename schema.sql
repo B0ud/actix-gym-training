def20b505ecc4a6c5dc55eef0f61f692a073d98e
BoudBoud47 <mehdi.boudart@gmail.com>
2021-01-09 17:36:30 +0100

    Change Database model -> Fix use underscores

--- schema.sql
+++ schema.sql
@@ -3,49 +3,49 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 
 
-CREATE TABLE MuscleTag (
-                             MuscleTagId UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+CREATE TABLE Muscle_Tag (
+                             Muscle_Tag_Id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
                              Name varchar(50) NOT NULL UNIQUE,
                              Code varchar(50) NOT NULL UNIQUE,
                              Description varchar(50),
                              Image varchar(50),
-                             CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-                             UpdatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+                             Created_At TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+                             Updated_At TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
 CREATE TABLE Exercise (
-                           ExerciseId UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+                           Exercise_Id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
                            Name varchar(50) NOT NULL UNIQUE,
                            Description varchar(50),
                            Category varchar(50) NOT NULL,
-                           CategoryIcon varchar(50),
+                           Category_Icon varchar(50),
                            Image varchar(50),
-                           CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-                           UpdatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+                           Create_dAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+                           Update_dAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
-CREATE TABLE ExerciseMuscleTag (
-                                MuscleTagId UUID REFERENCES MuscleTag (MuscleTagId),
-                                ExerciseId UUID REFERENCES Exercise(ExerciseId),
-                                CONSTRAINT Muscle_Tag_Exercice_pk PRIMARY KEY (ExerciseId, MuscleTagId)
+CREATE TABLE Exercise_Muscle_Tag (
+                                Muscle_Tag_Id UUID REFERENCES Muscle_Tag (Muscle_Tag_Id),
+                                Exercise_Id UUID REFERENCES Exercise(Exercise_Id),
+                                CONSTRAINT Muscle_Tag_Exercice_pk PRIMARY KEY (Exercise_Id, Muscle_Tag_Id)
 );
 
 CREATE TABLE Workout
 (
-    WorkoutId UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    Workout_Id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     "Date"    date DEFAULT CURRENT_DATE NOT NULL,
     "Type"      varchar(50) NOT NULL
 );
 
-CREATE TABLE WorkoutRep
+CREATE TABLE Workout_Rep
 (
-    WorkoutRepId     UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    ExerciseId       UUID REFERENCES Exercise (ExerciseId) NOT NULL,
-    WorkoutId        UUID REFERENCES Workout (WorkoutId) NOT NULL,
+    Workout_Rep_Id     UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    Exercise_Id       UUID REFERENCES Exercise (Exercise_Id) NOT NULL,
+    Workout_Id        UUID REFERENCES Workout (Workout_Id) NOT NULL,
     Rep              Integer NOT NULL,
-    DropSetRep       Integer NULL,
+    DropS_et_Rep       Integer NULL,
     Weight           Numeric NOT NULL,
-    WeightDropSetRep Numeric NULL
+    Weight_Drop_Set_Rep Numeric NULL
 );
 
 
