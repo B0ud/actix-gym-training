43cfed982b8c47ea5258c72105d5fa1aab7c0570
BoudBoud47 <mehdi.boudart@gmail.com>
2021-01-13 22:08:07 +0100

    Add some test

--- src/routes/exercises_routes.rs
+++ src/routes/exercises_routes.rs
@@ -12,7 +12,7 @@ async fn find(id: web::Path<Uuid>, db_pool: web::Data<PgPool>) -> impl Responder
         Ok(exercise) => HttpResponse::Ok().json(exercise),
         Err(e) => {
             error!("Error : {:?}", e);
-            HttpResponse::BadRequest().body("Exercise not found")
+            HttpResponse::NotFound().body("Exercise not found")
         }
     }
 }
@@ -39,7 +39,7 @@ pub fn init(cfg: &mut web::ServiceConfig) {
 mod tests {
     use super::*;
     use crate::config;
-    use actix_web::{test, App};
+    use actix_web::{test, App, http, web::Bytes};
     use dotenv::dotenv;
     use env_logger::{Target};
     use log::LevelFilter;
@@ -59,5 +59,48 @@ mod tests {
         let resp = test::call_service(&mut app, req).await;
         println!(" Response : {:?}", resp);
         assert!(resp.status().is_success());
+    }
+
+    #[actix_rt::test]
+    async fn it_test_get_one_exercise_by_invalid_uuid() {
+        dotenv().ok();
+        env_logger::Builder::new()
+            .filter(None, LevelFilter::Debug)
+            .target(Target::Stdout)
+            .init();
+
+        let db_pool = config::get_db_pool().await.unwrap();
+        let mut app = test::init_service(App::new().data(db_pool.clone()).configure(init)).await;
+
+        let req = test::TestRequest::get().uri("/exercises/wrong-id").to_request();
+        let resp = test::call_service(&mut app, req).await;
+
+        assert_eq!(resp.status(), http::StatusCode::NOT_FOUND);
+        let body = test::read_body(resp).await;
+        //println!(" Response : {:?}", resp);
+
+
+        assert_eq!(body,  Bytes::from_static(b"invalid length: expected one of [36, 32], found 8"));
+    }
+
+    #[actix_rt::test]
+    async fn it_test_get_one_exercise_by_not_found_id() {
+        dotenv().ok();
+        env_logger::Builder::new()
+            .filter(None, LevelFilter::Debug)
+            .target(Target::Stdout)
+            .init();
+
+        let db_pool = config::get_db_pool().await.unwrap();
+        let mut app = test::init_service(App::new().data(db_pool.clone()).configure(init)).await;
+
+        let req = test::TestRequest::get().uri("/exercises/f4fd5306-0fc1-44c7-a680-333333333333").to_request();
+        let resp = test::call_service(&mut app, req).await;
+
+        assert_eq!(resp.status(), http::StatusCode::NOT_FOUND);
+        let body = test::read_body(resp).await;
+        //println!(" Response : {:?}", resp);
+
+        assert_eq!(body,  Bytes::from_static(b"Exercise not found"));
     }
 }
