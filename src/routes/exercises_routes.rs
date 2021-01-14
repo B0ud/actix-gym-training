32d2853a69b36aecd19016566c7b3b1661460595
BoudBoud47 <mehdi.boudart@gmail.com>
2021-01-14 21:49:51 +0100

    Refactor tests

--- src/routes/exercises_routes.rs
+++ src/routes/exercises_routes.rs
@@ -39,20 +39,30 @@ pub fn init(cfg: &mut web::ServiceConfig) {
 mod tests {
     use super::*;
     use crate::config;
-    use actix_web::{test, App, http, web::Bytes};
+    use actix_web::{http, test, web::Bytes, App};
     use dotenv::dotenv;
-    use env_logger::{Target};
+    use env_logger::Target;
     use log::LevelFilter;
+    use sqlx::{Pool, Postgres};
+    use std::sync::Once;
+    static INIT: Once = Once::new();
+
+    pub async fn database_log_setup() -> Pool<Postgres> {
+        INIT.call_once(|| {
+            dotenv().ok();
+            env_logger::Builder::new()
+                .filter(None, LevelFilter::Debug)
+                .target(Target::Stdout)
+                .init()
+        });
+
+        let db_pool = config::get_db_pool().await.unwrap();
+        db_pool
+    }
 
     #[actix_rt::test]
     async fn it_test_get_all_exercises() {
-        dotenv().ok();
-        env_logger::Builder::new()
-            .filter(None, LevelFilter::Debug)
-            .target(Target::Stdout)
-            .init();
-
-        let db_pool = config::get_db_pool().await.unwrap();
+        let db_pool = database_log_setup().await;
         let mut app = test::init_service(App::new().data(db_pool.clone()).configure(init)).await;
 
         let req = test::TestRequest::get().uri("/exercises").to_request();
@@ -63,44 +73,38 @@ mod tests {
 
     #[actix_rt::test]
     async fn it_test_get_one_exercise_by_invalid_uuid() {
-        dotenv().ok();
-        env_logger::Builder::new()
-            .filter(None, LevelFilter::Debug)
-            .target(Target::Stdout)
-            .init();
-
-        let db_pool = config::get_db_pool().await.unwrap();
+        let db_pool = database_log_setup().await;
         let mut app = test::init_service(App::new().data(db_pool.clone()).configure(init)).await;
 
-        let req = test::TestRequest::get().uri("/exercises/wrong-id").to_request();
+        let req = test::TestRequest::get()
+            .uri("/exercises/wrong-id")
+            .to_request();
         let resp = test::call_service(&mut app, req).await;
 
         assert_eq!(resp.status(), http::StatusCode::NOT_FOUND);
         let body = test::read_body(resp).await;
         //println!(" Response : {:?}", resp);
 
-
-        assert_eq!(body,  Bytes::from_static(b"invalid length: expected one of [36, 32], found 8"));
+        assert_eq!(
+            body,
+            Bytes::from_static(b"invalid length: expected one of [36, 32], found 8")
+        );
     }
 
     #[actix_rt::test]
     async fn it_test_get_one_exercise_by_not_found_id() {
-        dotenv().ok();
-        env_logger::Builder::new()
-            .filter(None, LevelFilter::Debug)
-            .target(Target::Stdout)
-            .init();
-
-        let db_pool = config::get_db_pool().await.unwrap();
+        let db_pool = database_log_setup().await;
         let mut app = test::init_service(App::new().data(db_pool.clone()).configure(init)).await;
 
-        let req = test::TestRequest::get().uri("/exercises/f4fd5306-0fc1-44c7-a680-333333333333").to_request();
+        let req = test::TestRequest::get()
+            .uri("/exercises/f4fd5306-0fc1-44c7-a680-333333333333")
+            .to_request();
         let resp = test::call_service(&mut app, req).await;
 
         assert_eq!(resp.status(), http::StatusCode::NOT_FOUND);
         let body = test::read_body(resp).await;
         //println!(" Response : {:?}", resp);
 
-        assert_eq!(body,  Bytes::from_static(b"Exercise not found"));
+        assert_eq!(body, Bytes::from_static(b"Exercise not found"));
     }
 }
