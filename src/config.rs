cea2bfa36705a6b22036f4d3c32a6360c51942d4
BoudBoud47 <mehdi.boudart@gmail.com>
2021-02-18 21:24:35 +0100

    Fix import error import private module

--- src/config.rs
+++ src/config.rs
@@ -1,10 +1,9 @@
 //! Manage config
-use serde;
 use sqlx::{Error, PgPool, Pool, Postgres};
 use std::env;
 
 // Helper for init database Connection pool
-pub async fn get_db_pool() -> serde::export::Result<Pool<Postgres>, Error> {
+pub async fn get_db_pool() -> Result<Pool<Postgres>, Error> {
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file");
     let db_pool = PgPool::connect(&database_url);
     db_pool.await
